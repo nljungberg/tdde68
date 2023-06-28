89fbb97f2df22fe8f1e2a5887d9612aa1ed42d1d
Dag Jönsson <dag@jnsson.eu>
2023-06-28 09:00:37 +0200

    suppress warning about unused function, it's not a problem

--- userprog/process.c
+++ userprog/process.c
@@ -455,6 +455,10 @@ static bool install_page(void* upage, void* kpage, bool writable)
 		 && pagedir_set_page(t->pagedir, upage, kpage, writable));
 }
 
+// Don't raise a warning about unused function.
+// We know that dump_stack might not be called, this is fine.
+#pragma GCC diagnostic ignored "-Wunused-function"
+
 /* With the given stack pointer, will try and output the stack to STDOUT. */
 static void dump_stack(const void* esp)
 {
