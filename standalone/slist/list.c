78c12654945bde4b771f2ffcd988ea8515dbfaae
noalj314 <noalj314@student.liu.se>
2025-01-23 12:16:22 +0100

    stuff

--- standalone/slist/list.c
+++ standalone/slist/list.c
@@ -1,5 +1,35 @@
 #include "list.h"
+#include <stdlib.h>
+
+void append(struct list_item *first, int x)
+{
+    struct list_item *new_item = malloc(sizeof(struct list_item));
+    new_item->value = x;
+    new_item->next = NULL;
+    
+    if (first == NULL) {
+        first = new_item;
+        return;
+    }
+
+    struct list_item *current = first;
+    while(current->next != NULL) {
+        current = current->next;
+    }
+    current->next = new_item;
+}
+
+/* Puts value 'x' at the beginning of the list */
+void prepend(struct list_item *first, int x) {
+    struct list_item *new_item = malloc(sizeof(struct list_item));
+    new_item->value = x;
+    new_item->next = first;
+    first = new_item;
+}
 
 
+/* Find the first element in the list larger than x and
+ * put x right before that element */
+void input_sorted(struct list_item *first, int x) { 
 
-/* Implement the functions here */
+}
