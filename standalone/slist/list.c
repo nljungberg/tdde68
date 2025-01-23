f4bcb6ffbdac2e747259e210c0a6fd167da5943a
Tristan <trive080@student.liu.se>
2025-01-23 12:47:59 +0100

    Sorted append

--- standalone/slist/list.c
+++ standalone/slist/list.c
@@ -32,4 +32,32 @@ void prepend(struct list_item *first, int x) {
  * put x right before that element */
 void input_sorted(struct list_item *first, int x) { 
 
+    struct list_item *prev = malloc(sizeof(struct list_item));
+    struct list_item *new_item = malloc(sizeof(struct list_item));
+    struct list_item *current = malloc(sizeof(struct list_item));
+    new_item->value = x;
+    current = first;
+    prev = NULL;
+     if (first == NULL) {
+        new_item->next = NULL;
+        first = new_item;
+        return;
+    }
+     if (first->value > x) {
+        new_item->next = first;
+        first = new_item;
+        return;
+    }
+    while(current != NULL){
+        if(current->value > x){
+            new_item->next = current;
+            prev->next = next_item; 
+            return;
+        }
+        prev = current;
+        current = current->next;
+    }
+    prev->next = new_item;
+    new_item->next = NULL
 }
+
