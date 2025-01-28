bb151c303cf48ead0346920e315c7879e21fc1cb
noalj314 <noalj314@student.liu.se>
2025-01-28 14:58:43 +0100

    finished lab0

--- standalone/slist/list.c
+++ standalone/slist/list.c
@@ -39,32 +39,31 @@ void prepend(struct list_item *first, int x) {
  * put x right before that element */
 
 void input_sorted(struct list_item *first, int x) { 
-
-    struct list_item *new_item = malloc(sizeof(struct list_item));
-    new_item->value = x;
-    new_item->next = NULL;
-    struct list_item *current = first;
-     if (first == NULL || first->value > x) {
-        prepend(first,x);
+    if (first == NULL) {
         return;
-     }
-    while(current != NULL|| current->value > x){
-        if(current->next == NULL){
-            append(current, x);
-            return;}
+    }
+    if (x < first->value) {
+        prepend(first, x);
+        return;
+    }
+
+    struct list_item *current = first;
+    while (current->next != NULL && current->next->value <= x) {
         current = current->next;
     }
-    prepend(current, x);
+    struct list_item *new_item = malloc(sizeof(struct list_item));
+    new_item->value = x;
+    new_item->next = current->next;
+
+    current->next = new_item;
 }
 
 void print(struct list_item *first){
-     struct list_item *current;
-     current = first;
-    do{
-        printf("%i\n", current->value);
+    struct list_item *current = first;
+    while (current != NULL) {
+        printf("%d\n", current->value);
         current = current->next;
     }
-    while(current != NULL);
 }
 
 void clear(struct list_item* first){
