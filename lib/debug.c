207e14564f85f1ad0e799e3a06413116f529931d
Dag Jönsson <dag.jonsson@liu.se>
2025-02-05 15:55:56 +0100

    actually print entire callstack, not the last address

--- lib/debug.c
+++ lib/debug.c
@@ -5,32 +5,31 @@
 #include <stdio.h>
 #include <string.h>
 
-// Don't raise a warning about unsafe usage of __builtin_frame_address.
-// We know that we will have a calling function, so calling with id = 1 is safe.
-
 /* Prints the call stack, that is, a list of addresses, one in
-	each of the functions we are nested within.  gdb or addr2line
-	may be applied to kernel.o to translate these into file names,
-	line numbers, and function names.  */
-void debug_backtrace(void)
+   each of the functions we are nested within.  gdb or addr2line
+   may be applied to kernel.o to translate these into file names,
+   line numbers, and function names.  */
+void
+debug_backtrace (void)
 {
-	static bool explained;
-	void** frame;
+  static bool explained;
+  void **frame;
 
-	printf("Call stack: %p", __builtin_return_address(0));
-#pragma GCC diagnostic ignored "-Wframe-address"
-	for (frame = __builtin_frame_address(1);
-		  (uintptr_t) frame >= 0x1000 && frame[0] != NULL;
-		  frame = frame[0])
-#pragma GCC diagnostic pop
-		printf(" %p", frame[1]);
-	printf(".\n");
+  printf ("backtrace ");
+  for (frame = __builtin_frame_address (0);
+       frame != NULL && frame[0] != NULL;
+       frame = frame[0])
+    printf (" %p", frame[1]);
+  printf (".\n");
 
-	if (!explained) {
-		explained = true;
-		printf(
-			 "The `backtrace' program can make call stacks useful.\n"
-			 "Read \"Backtraces\" in the \"Debugging Tools\" chapter\n"
-			 "of the Pintos documentation for more information.\n");
-	}
+  if (!explained)
+    {
+      explained = true;
+      printf ("The `backtrace' program can make call stacks useful.\n"
+              "Read \"Backtraces\" in the \"Debugging Tools\" chapter\n"
+              "of the Pintos documentation for more information.\n"
+              "Simply copy-paste the backtrace command line above.\n"
+        );
+    }
 }
+
