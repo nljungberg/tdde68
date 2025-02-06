9203ac21852fc92190cfc4e7101cb66a6bf4bcb5
noalj314 <noalj314@su00-210.ad.liu.se>
2025-02-06 14:43:54 +0100

    lab2 almost done

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -20,6 +20,61 @@ void syscall_init(void)
 static void syscall_handler(struct intr_frame* f UNUSED)
 {
 	printf("system call!\n");
+	int syscall_nr = (f->esp);
+	int *args = (int *) f->esp;
+	switch (syscall_nr)
+	{
+	case SYS_WRITE:
+		write(args[1], args[2], args[3]);
+		/* code */
+		break;
+	case SYS_CLOSE:
+		close(args[1]);
+		/* code */
+		break;
+	case SYS_CREATE:
+		create(args[1], args[2]);
+		/* code */
+		break;
+	case SYS_HALT:	
+		halt();
+		/* code */
+		break;
+	case SYS_OPEN:
+		open(args[1]);
+		/* code */
+		break;
+	case SYS_READ:
+		read(args[1], args[2], args[3]);
+		/* code */
+		break;
+	case SYS_SLEEP:
+		sleep(args[1]);
+		/* code */
+		break;
+	case SYS_SEEK:
+		seek(args[1], args[2]);
+		/* code */
+		break;
+	case SYS_TELL:
+		tell(args[1]);
+		/* code */
+		break;
+	case SYS_REMOVE:
+		remove(args[1]);
+		/* code */
+		break;
+	case SYS_FILESIZE:
+		filesize(args[1]);
+		/* code */
+		break;
+	case SYS_EXIT:
+		exit(args[1]);
+		/* code */
+		break;
+	default:
+		break;
+	}
 	thread_exit();
 }
 
@@ -54,7 +109,7 @@ int open(const char *file){
 
 void close (int fd) {
 	struct thread *cur = thread_current();
-	if (fd > 128 || fd < 2) {
+	if ( 1 < fd < 128) {
 		return;
 	}
 	file_close(cur->fd_table[fd]);
@@ -65,18 +120,31 @@ int write (int fd, const void *buffer, unsigned size){
 	if (fd == 1) { // STDOUT
 		putbuf(buffer, size);
 		return size;
-	} else if (fd > 1) {
+	} else if ( 1 < fd < 128)  {
 		struct file *file = cur->fd_table[fd];
-		int write_id = file_write(file, buffer);
-		return write_id;
+		int write_size = file_write(file, buffer);
+		return write_size;
 	}
 	return -1;
 }
 
 int read (int fd, const void* buffer, unsigned size){
+
+	for (unsigned i = 0; i < size; i++) {  //checks that the buffer is valid
+			if(!is_user_vaddr(buffer + i)) {
+				return -1;
+			}
+		}
+
+	struct thread *cur = thread_current();
 	if (fd == 0) { // stdin
-		input_getc()
+		input_getc();
+	} else if (1 < fd < 128) {
+		struct file *file = cur->fd_table[fd];
+		int bytes_read = file_read(file, buffer, size);
+		return bytes_read;
 	}
+	return -1;
 
 }
 
@@ -85,17 +153,40 @@ bool remove(const char *file_name){
 }
 
 int filesize(int fd){
-
+	struct thread *cur = thread_current();
+	if (1 < fd < 128) {
+		struct file *file = cur->fd_table[fd];
+		return file_length(file);
+	}
+	return -1;
 }
 
-void seek(int fd){
+void seek(int fd, unsigned position){
+	struct thread *cur = thread_current();
+	if (1 < fd < 128) {
+		struct file *file = cur->fd_table[fd];
+		file_seek(file, position);
+	}
+	return -1;
 
 }
 
 unsigned tell(int fd){
+	struct thread *cur = thread_current();
+	if (1 < fd < 128) {
+		struct file *file = cur->fd_table[fd];
+		return file_tell(file);
+	}
+	return -1;
 
-}
+}	
 
 void exit(int status){
-	
+	struct thread *cur = thread_current();
+	cur->fd_table;
+	for (int i = 2; cur->fd_table[i] != NULL; i++) {
+		close(i);
+	}
+	cur->status = status;
+	thread_exit();
 }
